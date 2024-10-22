f417826646131959948b2174329cfbcc43e1ddb1
sefingeevarghese <138231966+sefingeevarghese@users.noreply.github.com>
2024-10-22 15:22:52 +0530

    second commit

--- Tops work/C programming/Assignment/Module 3 (C Language)/MODULE 3.2 (C Language Programing with C)/pattern 7.c
+++ Tops work/C programming/Assignment/Module 3 (C Language)/MODULE 3.2 (C Language Programing with C)/pattern 7.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main() 
 {
-    char i, j,count=65;
-    for (i=65; i<=68; i++) // rows
+    char i, j,count=69;
+    for (i=65; i<=69; i++) // rows
 	{
-        for (j=65;j<=i;j++) 	// columns
+        for (j=69;j<=i;j++) 	// columns
 		{
             printf("%c", count);
             count++;
